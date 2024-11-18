71b9d006b70f931b1e90f175f02239e7dba2b1b1
HURKAPARIZSI <kocsiskornel800@gmail.com>
2024-11-18 10:38:55 +0100

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal(id) {
+    document.getElementById(id).style.display = "flex";
+}
+
+// Modal bezárása
+function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+}
