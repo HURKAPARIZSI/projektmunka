15ac342442e754ea28b54f8c595e4a08612d0f62
HURKAPARIZSI <kocsiskornel800@gmail.com>
2024-11-18 10:49:44 +0100

    .

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function openModal(id) {
     document.getElementById(id).style.display = "flex";
 }
 
-// Modal bezárása
+
 function closeModal(id) {
     document.getElementById(id).style.display = "none";
 }
