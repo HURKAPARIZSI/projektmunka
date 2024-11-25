653446570a9a2d26ec933abb629f62ba8f6375e9
HURKAPARIZSI <kocsiskornel800@gmail.com>
2024-11-25 10:27:26 +0100

    .

--- Backend/includes/db.php
+++ Backend/includes/db.php
@@ -1,10 +1,14 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=erettsegiprojekt;charset=utf8mb4"; // Igazítsd az adatbázis adatokhoz
+$dsn = "mysql:host=localhost;dbname=eretsegiprojekt;charset=utf8mb4"; // Igazítsd az adatbázis adatokhoz
 $username = "root"; // Az adatbázis felhasználónév
 $password = "";     // Az adatbázis jelszó
 
+
+
 try {
     $pdo = new PDO($dsn, $username, $password);
+    $stmt = $pdo->("SELECT * FROM orders");
+    var_dump($stmt);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Adatbázis hiba: " . $e->getMessage());
